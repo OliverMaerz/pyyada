10ea4ba033cf2576b57952baf62bceb3e4c51c3c
Oliver Maerz <github@olivermaerz.com>
2016-12-09 01:13:30 -0300

    Add templates

--- main.py
+++ main.py
@@ -1,38 +1,46 @@
-# Copyright 2016 Google Inc.
+# main script
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
 
 import os
 import webapp2
 import jinja2
 
 template_dir = os.path.join(os.path.dirname(__file__), 'templates')
-jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir))
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),
+                               autoescape = True)
 
-class MainPage(webapp2.RequestHandler):
+
+class Handler(webapp2.RequestHandler):
+    def write(self, *a, **kw):
+        self.response.out.write(*a, **kw)
+
+    def render_str(self, template, **params):
+        t = jinja_env.get_template(template)
+        return t.render(params)
+
+    def render(self, template, **kw):
+        self.write(self.render_str(template, **kw))
+
+
+
+class MainPage(Handler):
     def get(self):
+        self.render("front.html", greeting = "Hello")
+
+    def render_front(self, title = "", art = "", error = ""):
+        self.render("front.html", title = title, art = art, error = error)
         #self.response.headers['Content-Type'] = 'text/html'
-        self.response.write('Hello, Fucking World!')
+        #self.response.write('Hello, Fucking World!')
 
-class Admin(webapp2.RequestHandler):
+class Login(Handler):
     def get(self):
-        q= self.request.get("q")
-        self.response.write(q)
+        q = self.request.get("q")
+        self.render("front.html", greeting = q)
 
 
 
 app = webapp2.WSGIApplication([
     ('/', MainPage),
-    ('/admin', Admin),
+    ('/login', Login),
 ], debug=True)
 
